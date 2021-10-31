c03c8eddac9399e166c6a489a25ec5a561ff534b
tromao <tiagomromao@gmail.com>
2021-10-31 17:50:50 +0000

    Easy example based on stev youtube

---
+++ app/src/main/java/com/example/bottomnavbardemo/screens/HomeScreen.kt
@@ -0,0 +1,33 @@
+package com.example.bottomnavbardemo.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Magenta)
+    ) {
+
+        Text(
+            text = "HOME",
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+
+        )
+    }
+}
+
+
+
